5b91a8bf41720956d5be502a92b6cba9d9e02968
sashaweiss <takanuva96@gmail.com>
2016-02-27 16:02:40 -0600

    grabs profile info

---
+++ client/views/editProfile.js
@@ -0,0 +1,16 @@
+Template.editProfile.events({
+    'submit #profile': function(e){
+        e.preventDefault();
+
+        var myprofile = {
+            'firstname': $('#first_name').val(),
+            'lastname':  $('#last_name').val(),
+            'phone':     $('#icon_telephone').val(),
+            'gender':    $("input:radio[name='gender']:checked").val(),
+            'notify':    $(e.target).find('[name=receiveNotifications]').val(),
+        }
+
+        console.log(myprofile);
+
+    }
+});
