99e237c7e87038526226074a905debd9e83df4a7
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-03-01 00:39:31 -0600

    Dongle

--- client/views/getInvolved.js
+++ client/views/getInvolved.js
@@ -1,1 +1,11 @@
-
+Template.getInvolved.events({
+    'click #donate': function(e){
+        alert('Thank you for donating!');
+    },
+    'click #canvass': function(e){
+        alert('Thank you for canvassing!');
+    },
+    'click #volunteer': function(e){
+        alert('Thank you for volunteering!');
+    },
+});
