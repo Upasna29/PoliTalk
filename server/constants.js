401c8fbac1a657c8565b8e4593467f598ba29a32
sashaweiss <takanuva96@gmail.com>
2016-02-29 20:14:54 -0600

    ?

--- server/constants.js
+++ server/constants.js
@@ -1,10 +1,26 @@
 if (Events.find().count()==0){
-    var event = {
-        'host':'Jan Schakowsky',
-        'location': 'ETHS',
-        'style': 'Town Hall',
-        'time': new Date(2016, 2, 4),
-        'description': "It's gonna be lit fam",
-    };
-    var id = Events.insert(event);
+    var events = [
+        {
+            'host':'Jan Schakowsky',
+            'location': 'ETHS',
+            'style': 'Town Hall',
+            'time': new Date(2016, 2, 4),
+            'description': "It's gonna be lit fam",
+        },
+        {
+            'host': 'Daniel Biss',
+            'location': 'Niles North High School',
+            'style': 'Speech',
+            'time': new Date(2016, 3, 1),
+            'description': "I know math vote for me"
+        },
+        {
+            'host': 'Bill Nye',
+            'location': 'James Park',
+            'style': 'Q & A',
+            'time': new Date(2016, 3, 16),
+            'description': "Oh man Bill Nye is speaking on my birthday! -Sasha"
+        }
+    ];
+    var id = Events.insert(events);
 }
